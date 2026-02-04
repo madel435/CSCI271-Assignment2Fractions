6ddb9098d71db54c16bc4494c7328686ac1c8f9a
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-04 16:59:45 -0500

    Added if /else if / else

--- CSCI271-Assignment2Fractions.java
+++ CSCI271-Assignment2Fractions.java
@@ -1,52 +1,64 @@
-public class CSCI271-Assignment2Fractions {
-  public class Fraction
-  {
-    private long numerator; 
-    private long denominator;
-    
-  }
-public Fraction(long numerator, long denominator)
+public class CSCI271-Assignment2Fractions 
 {
-  return 1;
-} 
-  public Fraction(int n){
-    numerator = num;
-    denominator = 1; 
-  }
-  public long Getnum(){
-    return 1;
-  }
-  public long Getdenom(){
-
-    return 1;
-    
-  }
+    public class Fraction
+    {
+      private long numerator; 
+      private long denominator;
+    }
+    public Fraction(long numerator, long denominator)
+    {
+      return 1;
+    } 
+    public Fraction(int n)
+    {
+      numerator = num;
+      denominator = 1; 
+    }
+  public long Getnum()
+    {
+      return 1;
+    }
+  public long Getdenom()
+    {
+        return 1;
+    }
   public Fraction add(Fraction a , Fraction b )
-  {
-    return a + b;
-  }
+    {
+      return a + b;
+    }
   
-public Fraction multiply(Fraction a , Fraction b )
-{
-  return a * b;
-}
-public Fraction divide(Fraction a , Fraction b )
-{
-  return a / b ;
-}
-public Fraction negate(Fraction a , Fraction b )
-{
-  return (a/b) * (b/a);
-}
-    public Fraction pow(Fraction a , Fraction b ){
+  public Fraction multiply(Fraction a , Fraction b )
+    {
+      return a * b;
+    }
+  public Fraction divide(Fraction a , Fraction b )
+    {
+      return a / b ;
+    }
+  public Fraction negate(Fraction a , Fraction b )
+    {
+      return (a/b) * (b/a);
+    }
+      public Fraction pow(Fraction a , Fraction b ){
       return 1 ;
     }
     public String toString(){
+      if (a==b)
+      { return "1";}
+      else if (b == 0)
+      {
+        return "Not a number. Cannot be +infintiy symbol, -infinity symbol, or 0 which is not a number"; 
+      }
+      else if 
+      {
+        return fraction f;
+      }
       System.out.println();
     }
     public Fraction divide(Fraction f )
-      {
+    {
       //Only works if data field is public 
       return new Fraction (*/Logic using Getters*/);
-      }
+    }
+}
 }
