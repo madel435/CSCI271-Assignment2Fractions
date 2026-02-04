7a68078f45676441681ec44b3c41cab4ef1a822b
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-04 16:54:39 -0500

    Added comments
    Refactor Fraction class methods to return 1 and add comments.

--- CSCI271-Assignment2Fractions.java
+++ CSCI271-Assignment2Fractions.java
@@ -7,17 +7,18 @@ public class CSCI271-Assignment2Fractions {
   }
 public Fraction(long numerator, long denominator)
 {
-  
+  return 1;
 } 
   public Fraction(int n){
     numerator = num;
     denominator = 1; 
   }
   public long Getnum(){
-    
+    return 1;
   }
   public long Getdenom(){
 
+    return 1;
     
   }
   public Fraction add(Fraction a , Fraction b )
@@ -38,14 +39,14 @@ public Fraction negate(Fraction a , Fraction b )
   return (a/b) * (b/a);
 }
     public Fraction pow(Fraction a , Fraction b ){
-      return ;
+      return 1 ;
     }
     public String toString(){
       System.out.println();
     }
     public Fraction divide(Fraction f )
       {
-      Only works if data field is public 
+      //Only works if data field is public 
       return new Fraction (*/Logic using Getters*/);
       }
 }
