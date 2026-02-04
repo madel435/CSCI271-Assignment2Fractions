631085bcf295f572867fa64f314f02efd0df869c
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-04 16:39:48 -0500

    Fraction class implementation

---
+++ CSCI271-Assignment2Fractions.java
@@ -0,0 +1,31 @@
+public class CSCI271-Assignment2Fractions {
+
+  public Fraction add(Fraction a , Fraction b )
+  {
+    return a + b;
+  }
+  
+public Fraction multiply(Fraction a , Fraction b )
+{
+  return a * b;
+}
+public Fraction divide(Fraction a , Fraction b )
+{
+  return a / b ;
+}
+public Fraction negate(Fraction a , Fraction b )
+{
+  return (a/b) * (b/a);
+}
+    public Fraction pow(Fraction a , Fraction b ){
+      return ;
+    }
+    public String toString(){
+      System.out.println();
+    }
+    public Fraction divide(Fraction f )
+      {
+      Only works if data field is public 
+      return new Fraction (*/Logic using Getters*/);
+      }
+}
