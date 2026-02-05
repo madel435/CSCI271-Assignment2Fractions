479888ec2283a9e6c74c2402e92cf6828857509e
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-04 22:28:21 -0500

    Added main into same file

--- Fraction.java
+++ Fraction.java
@@ -92,5 +92,18 @@ public class Fraction
                 return numerator + "/" + denominator; 
              }
           }
-          }
-   
+}
+
+//Main function 
+
+public static void main(String[] args)
+{
+  new Function(3,4);
+  new Function(0,0);
+  new Function(0,9);
+  new Function(9,0);
+  new Function(4,8);
+  new Function(5,9);
+  new Function(200/100);
+
+}
