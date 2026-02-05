4a4ad853950ba0a2887e1752fc01cf772258ea62
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-04 22:41:27 -0500

    } not the issue testing something from slides ughhh

--- Fraction.java
+++ Fraction.java
@@ -96,13 +96,12 @@ public class Fraction
    //Main function 
    public static void main(String[] args)
    {
-     new Function(3,4);
-     new Function(0,0);
-     new Function(0,9);
-     new Function(9,0);
-     new Function(4,8);
-     new Function(5,9);
-     new Function(200/100);
-   
+       System.out.println(new Function(3,4));
+       System.out.println(new Function(0,0));
+       System.out.println(new Function(0,9));
+       System.out.println(new Function(9,0));
+       System.out.println(new Function(4,8));
+       System.out.println(new Function(5,9));
+       System.out.println(new Function(200/100));
    }
 }
